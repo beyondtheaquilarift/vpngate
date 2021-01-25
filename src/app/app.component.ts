489277665f414d47a3b8cedecfe042786c00b0f9
greyheadedalbatross <greyheadedalbatross@protonmail.com>
2021-01-25 21:20:45 +0800

    add download openvpn file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import {Component, ViewChild, OnInit} from '@angular/core';
-import {ServerService} from './server.service';
-import {MatTableDataSource} from '@angular/material/table';
-import {MatSort} from '@angular/material/sort';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { ServerService } from './server.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import * as fileSaver from 'file-saver';
+import { toBase64String } from '@angular/compiler/src/output/source_map';
 
 @Component({
   selector: 'app-root',
@@ -28,5 +30,13 @@ export class AppComponent implements OnInit {
         this.dataSource = new MatTableDataSource(data);
         this.dataSource.sort = this.sort;
       });
+  }
+
+  downloadOvpnFile(configData, ip) {
+    console.log(window.atob(configData));
+    let config: string[] = [atob(configData)];
+    let blob = new Blob(config, { type: 'text/plain; charset=utf-8' });
+    let fileName = 'VPNGATE_'+ip+'.ovpn';
+    fileSaver.saveAs(blob, fileName);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { MatTableModule } from '@angular/material/table'
 import { MatSortModule } from '@angular/material/sort';
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RelativeTimePipe } from './relative-time.pipe';
-import {NgPipesModule} from 'ngx-pipes';
+import { NgPipesModule } from 'ngx-pipes';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,9 @@ import {NgPipesModule} from 'ngx-pipes';
     BrowserAnimationsModule,
     MatTableModule,
     MatSortModule,
+    MatIconModule,
     NgPipesModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
